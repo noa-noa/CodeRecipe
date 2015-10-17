6634c4242d8e7e42e333aa2f81170277be38e64a
Yuta Ide <yIde4949@gmail.com>
2015-10-18 03:16:46 +0900

    viewのサンプルができました. CSSを使うためにアセットパイプのtreeをdirectoryに書き換えました

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017143609) do
+ActiveRecord::Schema.define(version: 20151017151444) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
