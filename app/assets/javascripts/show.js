04fea0bc2d0b9fc34a5b60aec2a281f0772866da
Yuta Ide <yIde4949@gmail.com>
2015-10-18 10:44:32 +0900

    showページができた

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,1 +1,19 @@
-alert("afes")
+$(document).ready(function(){
+	var content = $("#restore").text();
+	html = $.parseHTML(content);
+	$("#restore").text("");
+	$("#restore").append(html)
+	var h2_list = $("#restore").children('h2');
+	$.each(h2_list, function(i){
+		$(this).attr("id", "h2_" + String(i));
+	});
+	console.log(html);
+
+	
+	console.log(h2_list.get(0));
+
+	$.each(h2_list, function(i){
+		$("#nav-col").children('ul').append("<li><a href = #h2_"+ i + ">" + $(this).text() + "</a></li>");
+
+	});
+});
